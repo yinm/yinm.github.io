070639e6e0887d2adddcc47217facd253e7f3367
yinm <yinm.cminor9@gmail.com>
2016-06-10 20:34:04 +0900

    portfolioのナビゲーション完成

---
+++ assets/js/min/script.js
@@ -0,0 +1,1 @@
+$(function(){var o=$("#global-nav");$(window).scroll(function(){console.log($(window).scrollTop()),$(window).scrollTop()>100?o.addClass("fixed"):o.removeClass("fixed")})});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 $(function() {
-  var $nav = $('global-nav');
+  var $nav = $('#global-nav');
 
   $(window).scroll(function() {
     console.log($(window).scrollTop());
-    if ($(window).scrollTop() > 1169) {
+    if ($(window).scrollTop() > 100) {
       $nav.addClass('fixed');
     }
     else {
