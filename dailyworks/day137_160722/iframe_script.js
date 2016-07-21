8629ce833467bd3f6d31abbdc5128656d5a8628f
yinm <yinm.cminor9@gmail.com>
2016-07-22 07:53:10 +0900

    #136参考追加  #137 jQuery scroll

---
+++ dailyworks/day137_160722/iframe_script.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("p").clone().appendTo(document.body);
+  $("p").clone().appendTo(document.body);
+  $("p").clone().appendTo(document.body);
+
+  $(window).scroll(function() {
+    $("span").css("display", "inline").fadeOut("slow");
+  });
+});
