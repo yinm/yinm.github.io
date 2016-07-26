4e7a7a46de8697e1625d52e4aa9c3ec27d4e895f
yinm <yinm.cminor9@gmail.com>
2016-07-26 20:41:09 +0900

    #141 introductory WebVR2

--- dailyworks/day140_160725/js/script.js
+++ dailyworks/day140_160725/js/script.js
@@ -40,6 +40,56 @@
     scene.add(light);
     scene.add(mesh);
 
+
+    // Skybox
+var cubeMap = new THREE.CubeTexture([]);
+cubeMap.format = THREE.RGBFormat;
+cubeMap.flipY = false;
+
+var loader = new THREE.ImageLoader();
+loader.load('../texture/skyboxsun25degtest.png', function (image) {
+    var getSide = function (x, y) {
+        var size = 1024;
+        var canvas = document.createElement('canvas');
+        canvas.width  = size;
+        canvas.height = size;
+
+        var context = canvas.getContext('2d');
+        context.drawImage(image, -x * size, -y * size);
+
+        return canvas;
+    }
+
+    cubeMap.images[0] = getSide(2, 1);
+    cubeMap.images[1] = getSide(0, 1);
+    cubeMap.images[2] = getSide(1, 0);
+    cubeMap.images[3] = getSide(1, 2);
+    cubeMap.images[4] = getSide(1, 1);
+    cubeMap.images[5] = getSide(3, 1);
+    cubeMap.needsUpdate = true;
+});
+
+var cubeShader = THREE.ShaderLib['cube'];
+cubeShader.uniforms['tCube'].value = cubeMap;
+
+var skyboxMaterial = new THREE.ShaderMaterial({
+    fragmentShader: cubeShader.fragmentShader,
+    vertexShader: cubeShader.vertexShader,
+    uniforms: cubeShader.uniforms,
+    depthWrite: false,
+    side: THREE.BackSide
+});
+
+var skybox = new THREE.Mesh(
+    new THREE.BoxGeometry(10000, 10000, 10000),
+    skyboxMaterial
+);
+
+scene.add(skybox);
+
+
+
+
     // VRControls
     const controls = new THREE.VRControls(camera);
 
@@ -70,6 +120,7 @@
     document.getElementById("btn").addEventListener("click", function() {
       effect.setFullScreen(true);
     }, false);
-  });
 
+
+  });
 }());
