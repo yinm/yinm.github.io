da50a674f4c2fd3e47ac842af816fa027345f1d8
yinm <yinm.cminor9@gmail.com>
2016-08-16 20:52:28 +0900

    #162 Ruby practice

---
+++ dailyworks/day162_160816/practice.rb
@@ -0,0 +1,18 @@
+# 参考：ドットインストール http://dotinstall.com/lessons/basic_ruby_v3/37108
+
+colors = ["red", "blue", "yellow"]
+
+# p colors[0]
+# p colors[-1]
+# p colors[0..2]
+# p colors[0...2]
+# p colors[5]
+
+# colors[0] = "pink"
+# colors[1..2] = ["white", "black"]
+# colors.push("gold")
+# colors << "silver"
+# p colors
+
+p colors.size
+p colors.sort
