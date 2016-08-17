60d1fdbc552bd9c728a509cdd939671d819acf18
yinm <yinm.cminor9@gmail.com>
2016-08-17 12:56:20 +0900

    #163 Ruby practice2

---
+++ dailyworks/day163_160817/practice.rb
@@ -0,0 +1,12 @@
+# 参考：ドットインストール http://dotinstall.com/lessons/basic_ruby_v3/37113
+score = gets.to_i
+
+# if score > 80
+#   puts "great!"
+# elsif score > 60
+#   puts "good!"
+# else
+#   puts "so so..."
+# end
+
+puts "great!" if score > 80
