e4d9546d108b48dfe4863c8ee306d8a1ff25637e
yinm <yinm.cminor9@gmail.com>
2016-09-30 14:14:31 +0900

    #207 preventDefault with Javascript2

---
+++ dailyworks/day207_160930/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+window.addEventListener('load', ()=> {
+  const link = document.getElementById('link');
+  link.addEventListener('click', (e)=> {
+    e.preventDefault();
+  }, false);
+
+}, false);
