d2195471961707566121bb298c8d9291d830dd02
yinm <yinm.cminor9@gmail.com>
2016-11-17 08:25:13 +0900

    #255 PHP OPP practice6

---
+++ dailyworks/day255_161117/index.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace EcSite\Model\Shop;
+
+class FoodShop
+{
+
+}
+
+class FurnitureShop
+{
+
+}
+
+class ToyShop
+{
+
+}
+
+class PlantShop
+{
+
+}
+
+class ComputerShop
+{
+
+}
+
+use EcSite\Model\Shop;
+
+$foodShop = new Shop\FoodShop();
+$furnitureShop = new Shop\FurnitureShop();
+$toyShop = new Shop\ToyShop();
+$plantShop = new Shop\PlantShop();
+$computerShop = new Shop\ComputerShop();
