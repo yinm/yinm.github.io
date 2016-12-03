3efda8c6c3acf688d35a60aa571712f7b37bc702
yinm <yinm.cminor9@gmail.com>
2016-12-03 21:27:26 +0900

    #271 learn C5

---
+++ dailyworks/day271_161203/learn.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int suuti;
+    scanf("%d", &suuti);
+
+    if (suuti == 10) {
+        printf("入力値は10です\n");
+    } else {
+        printf("入力値は10ではありません。\n");
+    }
+}
